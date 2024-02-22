8bb637830ee6e9eec62db10bc9f7cfc07096687f
Bruno214 <bruno214215216217218@gmail.com>
2024-02-22 11:39:22 -0300

    primeiro commit

---
+++ src/main/java/br/com/alura/fipefinder/FipefinderApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.fipefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipefinderApplication.class, args);
+	}
+
+}
